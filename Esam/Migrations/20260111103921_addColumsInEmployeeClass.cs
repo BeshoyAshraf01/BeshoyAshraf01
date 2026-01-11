e2e1b64d964b20f4f9cfb2fc17fc14a308ea07ae
BeshoyAshraf01 <beshoyashraf262@gmail.com>
2026-01-11 20:18:43 +0200

    upload image

---
+++ Esam/Migrations/20260111103921_addColumsInEmployeeClass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Esam.Migrations
+{
+    /// <inheritdoc />
+    public partial class addColumsInEmployeeClass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CreatedBy",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedOn",
+                table: "Employees",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedOn",
+                table: "Employees",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Employees",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UpdateBy",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "UpdatedOn",
+                table: "Employees",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedOn",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedOn",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "UpdateBy",
+                table: "Employees");
+
+            migrationBuilder.DropColumn(
+                name: "UpdatedOn",
+                table: "Employees");
+        }
+    }
+}
